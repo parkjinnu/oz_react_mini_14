049a92e515bf983cf222d8fe34955e864a08e26f
박진우 <jamguda010@gmail.com>
2025-11-21 13:07:21 +0900

    MovieCard, main 수정 및 useFanch.js 추가

--- src/assets/MovieCard.jsx
+++ src/assets/MovieCard.jsx
@@ -1,12 +1,21 @@
-const ItemList = () => {
+import { useNavigate } from "react-router-dom";
+
+const MovieCard = ({ movie }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-[150px] h-[220px] bg-blue-400 text-white flex flex-col border border-zinc-700">
-      <div className="flex-1 border-b border-zinc-700 flex items-center justify-center">
-        포스터
-      </div>
-      <div className="p-2 text-center">평점</div>
+    <div
+      className="p-4 bg-gray-800 rounded cursor-pointer"
+      onClick={() => navigate("/details")}
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+        className="rounded"
+      />
+      <h2 className="mt-2 text-white">{movie.title}</h2>
     </div>
   );
 };
 
-export default ItemList;
+export default MovieCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./Layout";
 import App from "./App";
+import MovieDetail from "./assets/MovieDetail";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      
+      <Route path="/" element={<Layout />}>
+        
+        
+        <Route index element={<App />} />
+               <Route path="details" element={<MovieDetail />} />
+                    </Route>
+                       </Routes>
+                          </BrowserRouter>
 );
+
