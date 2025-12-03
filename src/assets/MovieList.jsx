5e19e862a2b8782addd1ec8ee67cca95a99d2324
박진우 <jamguda010@gmail.com>
2025-12-03 13:12:35 +0900

    231

--- src/assets/MovieList.jsx
+++ src/assets/MovieList.jsx
@@ -1,6 +1,6 @@
 import MovieCard from "../components/MovieCard";
-import useFetch from "./useFetch";
-const MovieList = () => {
+import { useFetch } from "./useFetch";
+const { MovieList } = () => {
   const { movies } = useFetch();
 console.log(movies);
 
@@ -8,7 +8,7 @@ console.log(movies);
     <div className="bg-gray-900 min-h-screen p-10">
       <div className="grid grid-cols-5 gap-6 justify-items-center">
         {movies.length !== 0 &&
-        movies.results.map((movie, index) => (
+        movies.results.map((movie,   index) => (
           <MovieCard key={index} movie={movie} />
         ))}
       </div>
--- src/packges/App.jsx
+++ src/packges/App.jsx
@@ -1,18 +1,10 @@
-import React, { useState, useEffect } from "react";
-import movieListData from "./assets/movieListData.json";
-import MovieList from "./assets/MovieList";
+
 import "./app.css";
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    setMovies(movieListData.results);
-  }, []);
-
+  
   return (
     <div className="">
-      <MovieList movies={movies} />
     </div>
   );
 };
